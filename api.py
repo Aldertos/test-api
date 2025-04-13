746e29748bde02bb235e4804d13051e881c66afe
Aldertos <mentalitexd@gmail.com>
2025-04-13 14:54:20 +0300

    Create api.py

---
+++ api.py
@@ -0,0 +1,13 @@
+import requests
+
+user_id = int(input("Enter target ID: "))
+api_url = f"https://discord.com/api/v9/users/{user_id}/profile"
+token = "YOUR ACCOUNT TOKEN"
+
+
+headers = {
+  "Authorization": token,
+}
+
+response = requests.get(api_url, headers=headers)
+print(response.text)
